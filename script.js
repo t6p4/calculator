c36159c485144609bad0b52d0a8880ee4ffede1c
t6p4 <155362069+t6p4@users.noreply.github.com>
2024-01-05 10:56:06 +0200

    Add calculating functionality

---
+++ script.js
@@ -0,0 +1,93 @@
+let result = document.querySelector("p");
+let number = document.querySelectorAll(".number")
+let operator = document.querySelectorAll(".operator")
+let equals = document.querySelector(".equals")
+let clear = document.querySelectorAll("clear")
+
+result.textContent = 0
+
+let var1 = 0;
+let var2 = 0;
+let init = false
+let op = "";
+
+
+
+number.forEach((n) => {
+    n.addEventListener("click", (e) => {
+        if(!init) {
+           result.textContent = ""; 
+            result.textContent += Number(e.target.textContent)
+            init = true
+        }   else {
+            result.textContent += Number(e.target.textContent)
+        }  
+    })
+})
+
+operator.forEach((n) => {
+    n.addEventListener("click", (e) => {
+        if (var1 && !var2 && typeof result.textContent == "string" && !init) {
+            op = e.target.innerText
+            return;
+        }
+        if (!var1) {
+            var1 = Number(result.textContent);
+            op = e.target.innerText
+            
+            result.textContent = "";
+        } else {
+            var2 = Number(result.textContent)
+            result.textContent = ""
+            calculate(op);
+            op = e.target.innerText
+
+        }
+    })
+})
+
+function calculate(op, ...calc) {
+    calc.pop()
+    calc.push(var1, var2)
+    switch (op) {
+        case `+`:
+            result.textContent = calc.reduce((a, b) => a + b )
+            var1 = Number(result.textContent)
+            var2 = 0;
+            init = false
+            break;
+        case `-`:
+            result.textContent = calc.reduce((a, b) => a - b )
+            var1 = Number(result.textContent)
+            var2 = 0;
+            init = false
+            break
+        case `*`:
+            result.textContent = calc.reduce((a, b) => a * b )
+            var1 = Number(result.textContent)
+            var2 = 0;
+            init = false
+            break
+        case `/`:
+            result.textContent = calc.reduce((a, b) => a / b )
+            var1 = Number(result.textContent)
+            var2 = 0;
+            init = false
+            break
+        case `**`:
+            result.textContent = calc.reduce((a, b) => a ** b )
+            var1 = Number(result.textContent)
+            var2 = 0;
+            init = false
+            break
+        case `%`:
+            result.textContent = calc.reduce((a, b) => a % b )
+            var1 = Number(result.textContent)
+            var2 = 0;
+            init = false
+            break
+    }
+
+
+    
+}
